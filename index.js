2a6073fae85660f9990ce46740ff42da472fe5b2
eichisanden <eichisanden@gmail.com>
2018-02-04 08:24:34 +0900

    feature: search ua api

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 })
 
+const woothee = require('woothee');
+
+app.get('/api/ua', (req, res) => {
+  const q = req.query.q;
+  res.send(woothee.parse(q));
+})
+
 app.listen(process.env.PORT || 5000);
 
