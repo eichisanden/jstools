b6e8b28b34474b7c5698ae8b821f51ebed62a38e
eichisanden <>
2020-01-18 11:54:33 +0900

    add header dump method

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
-})
+});
 
 const woothee = require('woothee');
 
@@ -13,6 +13,10 @@ app.get('/api/ua', (req, res) => {
   const q = req.query.q;
   res.send(woothee.parse(q));
 })
+
+app.get('/headers', (req, res) => {
+  return res.send(JSON.stringify(req.headers));
+});
 
 app.listen(process.env.PORT || 5000);
 
